91672e7928c40f2cfbec3975cfc8af7338505ce6
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-01-26 21:19:14 -0500

    Add V BSP and seg stubs

--- vdoom/core/r_bsp.v
+++ vdoom/core/r_bsp.v
@@ -1,3 +1,28 @@
+@[has_globals]
 module core
 
-pub fn r_render_bsp() {}
+__global curline = &Seg(unsafe { nil })
+__global sidedef = &Side(unsafe { nil })
+__global linedef = &Line(unsafe { nil })
+__global frontsector = &Sector(unsafe { nil })
+__global backsector = &Sector(unsafe { nil })
+
+__global rw_x = 0
+__global rw_stopx = 0
+__global segtextured = false
+__global markfloor = false
+__global markceiling = false
+__global skymap = false
+
+__global drawsegs = []DrawSeg{len: maxdrawsegs}
+__global ds_p = &DrawSeg(unsafe { nil })
+
+__global hscalelight = []voidptr{}
+__global vscalelight = []voidptr{}
+__global dscalelight = []voidptr{}
+
+pub type DrawFunc = fn (start int, stop int)
+
+pub fn r_clear_clip_segs() {}
+pub fn r_clear_draw_segs() {}
+pub fn r_render_bsp_node(bspnum int) { _ = bspnum }
--- vdoom/core/r_segs.v
+++ vdoom/core/r_segs.v
@@ -1,3 +1,9 @@
 module core
 
 pub fn r_render_segs() {}
+
+pub fn r_render_masked_seg_range(ds &DrawSeg, x1 int, x2 int) {
+	_ = ds
+	_ = x1
+	_ = x2
+}
