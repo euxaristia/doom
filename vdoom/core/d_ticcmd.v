925a20017798c7a9056d207c3b0ac18fff4d1a17
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-01-26 19:26:37 -0500

    Add V ticcmd struct

---
+++ vdoom/core/d_ticcmd.v
@@ -0,0 +1,15 @@
+module core
+
+pub struct TicCmd {
+pub mut:
+	forwardmove i8
+	sidemove    i8
+	angleturn   i16
+	chatchar    u8
+	buttons     u8
+	consistancy u8
+	buttons2    u8
+	inventory   int
+	lookfly     u8
+	arti        u8
+}
