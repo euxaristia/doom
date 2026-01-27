5af414accfcd4fc42a70e4330df0564c949e94dd
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-01-26 21:20:28 -0500

    Add V plane and thing stubs

--- vdoom/core/r_plane.v
+++ vdoom/core/r_plane.v
@@ -1,3 +1,47 @@
+@[has_globals]
 module core
 
-pub fn r_render_planes() {}
+__global lastopening = []i16{}
+
+pub type PlaneFunction = fn (top int, bottom int)
+
+__global floorfunc = PlaneFunction(unsafe { nil })
+__global ceilingfunc = PlaneFunction(unsafe { nil })
+
+__global floorclip = []i16{len: screenwidth}
+__global ceilingclip = []i16{len: screenwidth}
+__global yslope = []Fixed{len: screenheight}
+__global distscale = []Fixed{len: screenwidth}
+
+pub fn r_init_planes() {}
+pub fn r_clear_planes() {}
+
+pub fn r_map_plane(y int, x1 int, x2 int) {
+	_ = y
+	_ = x1
+	_ = x2
+}
+
+pub fn r_make_spans(x int, t1 int, b1 int, t2 int, b2 int) {
+	_ = x
+	_ = t1
+	_ = b1
+	_ = t2
+	_ = b2
+}
+
+pub fn r_draw_planes() {}
+
+pub fn r_find_plane(height Fixed, picnum int, lightlevel int) &Visplane {
+	_ = height
+	_ = picnum
+	_ = lightlevel
+	return unsafe { nil }
+}
+
+pub fn r_check_plane(pl &Visplane, start int, stop int) &Visplane {
+	_ = pl
+	_ = start
+	_ = stop
+	return unsafe { nil }
+}
--- vdoom/core/r_things.v
+++ vdoom/core/r_things.v
@@ -1,3 +1,36 @@
+@[has_globals]
 module core
 
-pub fn r_render_things() {}
+pub const maxvissprites = 128
+
+__global vissprites = []VisSprite{len: maxvissprites}
+__global vissprite_p = &VisSprite(unsafe { nil })
+__global vsprsortedhead = VisSprite{}
+
+__global negonearray = []i16{len: screenwidth, init: -1}
+__global screenheightarray = []i16{len: screenwidth, init: i16(screenheight)}
+
+__global mfloorclip = []i16{}
+__global mceilingclip = []i16{}
+__global spryscale = Fixed(0)
+__global sprtopscreen = Fixed(0)
+__global pspritescale = Fixed(0)
+__global pspriteiscale = Fixed(0)
+
+pub fn r_draw_masked_column(column voidptr) {
+	_ = column
+}
+
+pub fn r_sort_vis_sprites() {}
+pub fn r_add_sprites(sec &Sector) { _ = sec }
+pub fn r_add_psprites() {}
+pub fn r_draw_sprites() {}
+pub fn r_init_sprites(namelist []string) { _ = namelist }
+pub fn r_clear_sprites() {}
+pub fn r_draw_masked() {}
+
+pub fn r_clip_vis_sprite(vis &VisSprite, xl int, xh int) {
+	_ = vis
+	_ = xl
+	_ = xh
+}
