0241d0a45566f4afcbd7210f8cce3086877a0eaf
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-02-19 02:34:19 -0500

    port input core: bind mouse/keyboard and joystick config variables

--- src/i_input.v
+++ src/i_input.v
@@ -3,9 +3,36 @@ module main
 
 // Input binding and text-input hooks.
 
+@[weak]
+__global (
+	mut text_input_enabled       = true
+	mut novert                  = int(1)
+	mut vanilla_keyboard_mapping = int(1)
+	mut mouse_acceleration      = f32(2.0)
+	mut mouse_threshold         = int(10)
+	mut mouse_acceleration_y    = f32(1.0)
+	mut mouse_threshold_y       = int(0)
+	mut mouse_y_invert          = int(0)
+	mut runcentering            = int(1)
+)
+
+@[c: 'M_BindFloatVariable']
+fn m_bind_float_variable(name &i8, variable &f32)
+
 @[export: 'I_BindInputVariables']
 pub fn i_bind_input_variables() {
-	// No-op placeholder.
+	m_bind_float_variable(c'mouse_acceleration', &mouse_acceleration)
+	m_bind_int_variable(c'mouse_threshold', &mouse_threshold)
+	m_bind_int_variable(c'vanilla_keyboard_mapping', &vanilla_keyboard_mapping)
+	m_bind_int_variable(c'novert', &novert)
+	m_bind_float_variable(c'mouse_acceleration_y', &mouse_acceleration_y)
+	m_bind_int_variable(c'mouse_threshold_y', &mouse_threshold_y)
+	m_bind_int_variable(c'mouse_y_invert', &mouse_y_invert)
+}
+
+@[export: 'I_BindStrifeInputVariables']
+pub fn i_bind_strife_input_variables() {
+	m_bind_int_variable(c'runcentering', &runcentering)
 }
 
 @[export: 'I_StartTextInput']
@@ -14,12 +41,12 @@ pub fn i_start_text_input(_x1 int, _y1 int, _x2 int, _y2 int) {
 	_ = _y1
 	_ = _x2
 	_ = _y2
-	// No-op placeholder.
+	text_input_enabled = true
 }
 
 @[export: 'I_StopTextInput']
 pub fn i_stop_text_input() {
-	// No-op placeholder.
+	text_input_enabled = false
 }
 
 @[export: 'I_Tactile']
--- src/i_joystick.v
+++ src/i_joystick.v
@@ -3,12 +3,65 @@ module main
 
 // Joystick binding and init hooks.
 
+const num_virtual_buttons = 17
+
+@[weak]
+__global (
+	mut joystick_initialized        bool
+	mut usejoystick                = int(0)
+	mut use_gamepad                = int(0)
+	mut gamepad_type               = int(0)
+	mut joystick_guid              = &u8(c'')
+	mut joystick_index             = int(-1)
+	mut joystick_x_axis            = int(0)
+	mut joystick_x_invert          = int(0)
+	mut joystick_y_axis            = int(1)
+	mut joystick_y_invert          = int(0)
+	mut joystick_strafe_axis       = int(-1)
+	mut joystick_strafe_invert     = int(0)
+	mut joystick_look_axis         = int(-1)
+	mut joystick_look_invert       = int(0)
+	mut joystick_x_dead_zone       = int(33)
+	mut joystick_y_dead_zone       = int(33)
+	mut joystick_strafe_dead_zone  = int(33)
+	mut joystick_look_dead_zone    = int(33)
+	mut use_analog                 = int(1)
+	mut joystick_turn_sensitivity  = int(10)
+	mut joystick_move_sensitivity  = int(10)
+	mut joystick_look_sensitivity  = int(10)
+	mut joystick_physical_buttons  = [num_virtual_buttons]int{init: index}
+)
+
 @[export: 'I_BindJoystickVariables']
 pub fn i_bind_joystick_variables() {
-	// No-op placeholder.
+	m_bind_int_variable(c'use_joystick', &usejoystick)
+	m_bind_int_variable(c'use_gamepad', &use_gamepad)
+	m_bind_int_variable(c'gamepad_type', &gamepad_type)
+	m_bind_string_variable(c'joystick_guid', &joystick_guid)
+	m_bind_int_variable(c'joystick_index', &joystick_index)
+	m_bind_int_variable(c'joystick_x_axis', &joystick_x_axis)
+	m_bind_int_variable(c'joystick_y_axis', &joystick_y_axis)
+	m_bind_int_variable(c'joystick_strafe_axis', &joystick_strafe_axis)
+	m_bind_int_variable(c'joystick_x_invert', &joystick_x_invert)
+	m_bind_int_variable(c'joystick_y_invert', &joystick_y_invert)
+	m_bind_int_variable(c'joystick_strafe_invert', &joystick_strafe_invert)
+	m_bind_int_variable(c'joystick_look_axis', &joystick_look_axis)
+	m_bind_int_variable(c'joystick_look_invert', &joystick_look_invert)
+	m_bind_int_variable(c'joystick_x_dead_zone', &joystick_x_dead_zone)
+	m_bind_int_variable(c'joystick_y_dead_zone', &joystick_y_dead_zone)
+	m_bind_int_variable(c'joystick_strafe_dead_zone', &joystick_strafe_dead_zone)
+	m_bind_int_variable(c'joystick_look_dead_zone', &joystick_look_dead_zone)
+	m_bind_int_variable(c'use_analog', &use_analog)
+	m_bind_int_variable(c'joystick_turn_sensitivity', &joystick_turn_sensitivity)
+	m_bind_int_variable(c'joystick_move_sensitivity', &joystick_move_sensitivity)
+	m_bind_int_variable(c'joystick_look_sensitivity', &joystick_look_sensitivity)
+	for i := 0; i < joystick_physical_buttons.len; i++ {
+		name := 'joystick_physical_button${i}'
+		m_bind_int_variable(name.str, &joystick_physical_buttons[i])
+	}
 }
 
 @[export: 'I_InitJoystick']
 pub fn i_init_joystick() {
-	// No-op placeholder.
+	joystick_initialized = usejoystick != 0
 }
