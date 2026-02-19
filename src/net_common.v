bc3458c76e332ec40165b7ab9b99c9b7f99d02c8
euxaristia <25621994+euxaristia@users.noreply.github.com>
2026-02-19 00:58:41 -0500

    cleanup net_common: remove duplicate query entrypoint exports

--- src/net_common.v
+++ src/net_common.v
@@ -27,18 +27,3 @@ pub fn net_open_log() {
 pub fn net_bind_variables_export() {
 	// No-op placeholder.
 }
-
-@[export: 'NET_LANQuery']
-pub fn net_lan_query_export() {
-	println('NET_LANQuery: not implemented in this port')
-}
-
-@[export: 'NET_MasterQuery']
-pub fn net_master_query_export() {
-	println('NET_MasterQuery: not implemented in this port')
-}
-
-@[export: 'NET_QueryAddress']
-pub fn net_query_address_export(addr &i8) {
-	println('NET_QueryAddress: ${cstring(addr)}')
-}
